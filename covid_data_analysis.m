5c9bf49159c61a1aceb72848202f152ceb724021
vepitzer <vepitzer@gmail.com>
2021-01-05 14:07:42 -0500

    Update covid_data_analysis.m

--- covid_data_analysis.m
+++ covid_data_analysis.m
@@ -1,7 +1,7 @@
 %% Data cleaning for data downloaded from covidtracking.org 
 
 % Define date range
-date=[2020*ones(28,1) 3*ones(28,1) (4:31)'; 2020*ones(30,1) 4*ones(30,1) (1:30)'; 2020*ones(31,1) 5*ones(31,1) (1:31)'; 2020*ones(30,1) 6*ones(30,1) (1:30)'; 2020*ones(12,1) 7*ones(12,1) (1:12)'];
+date=[2020*ones(28,1) 3*ones(28,1) (4:31)'; 2020*ones(30,1) 4*ones(30,1) (1:30)'; 2020*ones(31,1) 5*ones(31,1) (1:31)'; 2020*ones(30,1) 6*ones(30,1) (1:30)'; 2020*ones(31,1) 7*ones(31,1) (1:31)'; 2020*ones(30,1) 8*ones(30,1) (1:30)'];
 dateUS=datenum(date);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -10,7 +10,7 @@ dateUS=datenum(date);
 %% Use data import tool to import data from covidtracking.org
 npos=flipud(positiveUS);
 nneg=flipud(negativeUS);
-%nhosp=flipud(hospitalizedCumulative);
+nhosp=flipud(hospitalizedCumulative);
 
 %% Calculate number of new cases/tests per day and percent positive
 ntest=npos+nneg;
@@ -64,9 +64,20 @@ for i=1:1000
 end
 R0ci_US_test=prctile(R0est_US_test,[2.5 97.5])
 
-%% Estimate Rt for US data
+%% Estimate Rt for US data using Wallinga-Teunis
 [Rt_US,Rt_US_adj,Rt_US_ci]=wallinga_teunis(newcases,[4.79 1.357],1000);
 [Rt_US_tests,Rt_US_tests_adj]=wallinga_teunis(newtests,[4.79 1.357]);
+
+%% Estimate Rt for US data using EpiEstim
+config = make_config('mean_si',6.5,'std_si',4); %'wind_len',3,
+
+Rt_epiestim_US=estimate_R(newcases,'parametric_si',config);
+Rt_epiestim_UStests=estimate_R(newtests,'parametric_si',config);
+
+Rt_US_epiestim=Rt_epiestim_US.median_posterior';
+Rt_US_epiestim_ci=[Rt_epiestim_US.quantile_0025_posterior' Rt_epiestim_US.quantile_0975_posterior'];
+Rt_UStests_epiestim=Rt_epiestim_UStests.median_posterior';
+Rt_UStests_epiestim_ci=[Rt_epiestim_UStests.quantile_0025_posterior' Rt_epiestim_UStests.quantile_0975_posterior'];
 
 %% Final US figure
 figure
@@ -75,8 +86,8 @@ hold on
 [ax,y1,y2]=plotyy(dateUS,newtests,dateUS,newcases);
 set(y1,'Color','r','LineWidth',1)
 set(y2,'Color','b','LineWidth',1)
-set(ax(1),'XLim',[dateUS(1)-3 dateUS(end)-3],'XTick',[dateUS(1)-3; dateUS([59 120],1)],'XTickLabel',{'Mar-01','May-01','Jul-01'},'YColor','k','FontSize',8)
-set(ax(2),'XLim',[dateUS(1)-3 dateUS(end)-3],'XTick',[dateUS(1)-3; dateUS([59 120],1)],'XTickLabel',{'Mar-01','May-01','Jul-01'},'YColor','k','FontSize',8)
+set(ax(1),'XLim',[dateUS(1)-3 dateUS(end)+2],'XTick',[dateUS(1)-3; dateUS([59 120],1); dateUS(end)+2],'XTickLabel',{'Mar-01','May-01','Jul-01','Sep-01'},'YColor','k','FontSize',8)
+set(ax(2),'XLim',[dateUS(1)-3 dateUS(end)+2],'XTick',[dateUS(1)-3; dateUS([59 120],1); dateUS(end)+2],'XTickLabel',{'Mar-01','May-01','Jul-01','Sep-01'},'YColor','k','FontSize',8)
 ax(1).YLabel.String='# of tests';
 ax(2).YLabel.String='# of positive cases';
 legend('Tests','Positive cases')
@@ -104,8 +115,8 @@ plot(dateUS,100*ppos_mavg,'Color',[.5 0 .5],'LineWidth',1)
 ylabel('% positive')
 legend('Daily data','Cumulative data')
 datetick('x','mmm-dd-yy')
-xlim([dateUS(1)-3 dateUS(end)+1])
-set(gca,'XTick',[dateUS(1)-3; dateUS([59 120],1)],'XTickLabel',{'Mar-01','May-01','Jul-01'},'FontSize',8)
+xlim([dateUS(1)-3 dateUS(end)+2])
+set(gca,'XTick',[dateUS(1)-3; dateUS([59 120],1); dateUS(end)+2],'XTickLabel',{'Mar-01','May-01','Jul-01','Sep-01'},'FontSize',8)
 
 subplot(1,4,4)
 hold on
@@ -124,17 +135,17 @@ xlim([dateUS(1)-3 dateUS(end)-3])
 ylim([0 5])
 legend('Estimated \it{R_t}','Uncorrected','Corrected')
 %%
-gtext('A','FontWeight','bold','FontSize',8)
-gtext('B','FontWeight','bold','FontSize',8)
-gtext('C','FontWeight','bold','FontSize',8)
-gtext('D','FontWeight','bold','FontSize',8)
+gtext('A)','FontWeight','bold','FontSize',8)
+gtext('B)','FontWeight','bold','FontSize',8)
+gtext('C)','FontWeight','bold','FontSize',8)
+gtext('D)','FontWeight','bold','FontSize',8)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % STATE-LEVEL DATA %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Download the historical state data from covidtracking.org API
 % Import data using the data import tool 
-% Rename first 4 columns "daily_date", "datily_state", "positive", "negative"
+% Rename first 4 columns "daily_date", "daily_state", "positive", "negative"
 
 x=date(:,1)*10000+date(:,2)*100+date(:,3);
 z=categorical(states);
@@ -230,7 +241,7 @@ end
 R0ci_bystate_test=prctile(R0est_bystate_test,[2.5 97.5])';
 R0ci_cum_bystate_test=prctile(R0cum_bystate_test,[2.5 97.5])';
 
-%% Calculate and plot Rt for each state
+%% Calculate Rt for each state using Wallinga-Teunis
 Rt_bystate=zeros(size(newcases_bystate,1)-1,51);
 Rt_bystate_adj=zeros(size(newcases_bystate,1)-1,51);
 Rt_bystate_ci=zeros(size(newcases_bystate,1)-1,2,51);
@@ -239,6 +250,21 @@ Rt_bystate_tests_adj=zeros(size(newtests_bystate,1)-1,51);
 for j=1:51
     [Rt_bystate(:,j),Rt_bystate_adj(:,j),Rt_bystate_ci(:,:,j)]=wallinga_teunis(newcases_bystate(:,j),[4.79 1.357],1000);
     [Rt_bystate_tests(:,j),Rt_bystate_tests_adj(:,j)]=wallinga_teunis(newtests_bystate(:,j),[4.79 1.357]);
+end
+
+%% Estimate Rt for each state using EpiEstim
+config = make_config('mean_si',6.5,'std_si',4); %'wind_len',3,
+
+for j=1:51
+    Rt_epiestim_bystate(j)=estimate_R(newcases_bystate(:,j),'parametric_si',config);
+    Rt_epiestim_bystate_tests(j)=estimate_R(newtests_bystate(:,j),'parametric_si',config);
+end
+%%
+for j=1:51
+    Rt_bystate_epiestim(:,j)=Rt_epiestim_bystate(j).median_posterior';
+    Rt_bystate_epiestim_ci(:,:,j)=[Rt_epiestim_bystate(j).quantile_0025_posterior' Rt_epiestim_bystate(j).quantile_0975_posterior'];
+    Rt_bystate_tests_epiestim(:,j)=Rt_epiestim_bystate_tests(j).median_posterior';
+    Rt_bystate_tests_epiestim_ci(:,:,j)=[Rt_epiestim_bystate_tests(j).quantile_0025_posterior' Rt_epiestim_bystate_tests(j).quantile_0975_posterior'];
 end
 
 %% Final figure for select states
@@ -325,14 +351,14 @@ for i=1:n
 end
 
 %%
-gtext('A','FontWeight','bold','FontSize',8)
-gtext('B','FontWeight','bold','FontSize',8)
-gtext('C','FontWeight','bold','FontSize',8)
-gtext('D','FontWeight','bold','FontSize',8)
+gtext('A)','FontWeight','bold','FontSize',8)
+gtext('B)','FontWeight','bold','FontSize',8)
+gtext('C)','FontWeight','bold','FontSize',8)
+gtext('D)','FontWeight','bold','FontSize',8)
 
 %% Per capita testing rates and growth rates for select states
 for i=1:51
-    [st_test_r(:,i),~,st_test_rstats(i)]=glmfit((1:103)',10000*newtests_bystate(29:end,i)./pop_bystate(i),'Poisson');
+    [st_test_r(:,i),~,st_test_rstats(i)]=glmfit((1:91)',10000*newtests_bystate(29:119,i)./pop_bystate(i),'Poisson');
     [st_test_rinit(:,i),~,st_test_rinitstats(i)]=glmfit((t1(i):21)',10000*newtests_bystate(t1(i):21,i)./pop_bystate(i),'Poisson');
 end
 
@@ -353,12 +379,12 @@ title(states(st(i)))
 
 subplot(2,6,i+6)
 hold on
-plot(dateUS(29:end),glmval(st_test_r,(1:103)','log'),'Color',[.7 .7 .7])
+plot(dateUS(29:119),glmval(st_test_r,(1:91)','log'),'Color',[.7 .7 .7])
 plot(dateUS(29:end),10000*newtests_bystate(29:end,st(i))./pop_bystate(st(i)),'r')
-plot(dateUS(29:end),glmval(st_test_r(:,st(i)),(1:103)','log'),'k')
+plot(dateUS(29:119),glmval(st_test_r(:,st(i)),(1:91)','log'),'k')
 datetick('x','mmm-dd')
 xlim([dateUS(28) dateUS(end)+1])
-set(gca,'XTick',dateUS([29 90]),'XTickLabel',{'Apr-01','Jun-01'})
+set(gca,'XTick',dateUS([29 90 151]),'XTickLabel',{'Apr-01','Jun-01','Aug-01'})
 ylim([0 50])
 if i==1
 ylabel('# of tests (per 10K)')
@@ -366,20 +392,21 @@ end
 title(states(st(i)))
 end
 %%
-gtext('A','FontWeight','bold')
-gtext('B','FontWeight','bold')
+gtext('A)','FontWeight','bold')
+gtext('B)','FontWeight','bold')
 
 %% Plot Rt for all 51 US states and territories
 for j=1:51
     [Rt_bystate(:,j),Rt_bystate_adj(:,j)]=wallinga_teunis(newcases_bystate(:,j),[4.79 1.357]);
     [Rt_bystate_tests(:,j),Rt_bystate_tests_adj(:,j)]=wallinga_teunis(newtests_bystate(:,j),[4.79 1.357]);
 end
-
+%%
 figure
 for i=1:51
     subplot(9,6,i)
     hold on
-    [ax,y1,y2]=plotyy(dateUS(Rt_bystate(1:end-12,i)>0),Rt_bystate(Rt_bystate(1:end-12,i)>0,i),dateUS,100*ppos_bystate(:,i));
+    %[ax,y1,y2]=plotyy(dateUS(Rt_bystate(1:end-12,i)>0),Rt_bystate(Rt_bystate(1:end-12,i)>0,i),dateUS,100*ppos_bystate(:,i));
+    [ax,y1,y2]=plotyy(dateUS(Rt_epiestim_bystate(i).t_end'),Rt_bystate_epiestim(:,i),dateUS,100*ppos_bystate(:,i));
     set(y1(1),'Color',[0 .7 0],'LineWidth',1)
     set(y2(1),'Color',[.5 0 .5])
     set(ax(1),'YColor','k','FontSize',8)
@@ -388,10 +415,12 @@ for i=1:51
     if i==51
         legend('R_t','Percent positive')
     end
-    fill([dateUS(~isnan(Rt_bystate_ci(:,1,i)),1); flipud(dateUS(~isnan(Rt_bystate_ci(:,1,i)),1)); dateUS(find(~isnan(Rt_bystate_ci(:,1,i)),1))],[Rt_bystate_ci(~isnan(Rt_bystate_ci(:,1,i)),1,i); flipud(Rt_bystate_ci(~isnan(Rt_bystate_ci(:,1,i)),2,i)); Rt_bystate_ci(find(~isnan(Rt_bystate_ci(:,1,i)),1),1,i)],[.9 .9 .9],'EdgeColor',[.9 .9 .9])
+    %fill([dateUS(~isnan(Rt_bystate_ci(:,1,i)),1); flipud(dateUS(~isnan(Rt_bystate_ci(:,1,i)),1)); dateUS(find(~isnan(Rt_bystate_ci(:,1,i)),1))],[Rt_bystate_ci(~isnan(Rt_bystate_ci(:,1,i)),1,i); flipud(Rt_bystate_ci(~isnan(Rt_bystate_ci(:,1,i)),2,i)); Rt_bystate_ci(find(~isnan(Rt_bystate_ci(:,1,i)),1),1,i)],[.9 .9 .9],'EdgeColor',[.9 .9 .9])
     %plot(dateUS(Rt_bystate_tests_adj(1:end-1,i)>0),Rt_bystate_tests_adj(Rt_bystate_tests_adj(1:end-1,i)>0,i),'r')
-    plot(dateUS(Rt_bystate_adj(1:end-1,i)>0),Rt_bystate_adj(Rt_bystate_adj(1:end-1,i)>0,i),'k')
-    plot(dateUS(Rt_bystate(1:end-12,i)>0),Rt_bystate(Rt_bystate(1:end-12,i)>0,i),'Color',[0 .7 0],'LineWidth',1)
+    %plot(dateUS(Rt_bystate_adj(1:end-1,i)>0),Rt_bystate_adj(Rt_bystate_adj(1:end-1,i)>0,i),'k')
+    %plot(dateUS(Rt_bystate(1:end-12,i)>0),Rt_bystate(Rt_bystate(1:end-12,i)>0,i),'Color',[0 .7 0],'LineWidth',1)
+    fill([dateUS(Rt_epiestim_bystate(i).t_end'); dateUS(flipud(Rt_epiestim_bystate(i).t_end')); dateUS(Rt_epiestim_bystate(i).t_end(1))],[Rt_bystate_epiestim_ci(:,1,i); flipud(Rt_bystate_epiestim_ci(:,2,i)); Rt_bystate_epiestim_ci(1,1,i)],[.9 1 .9],'EdgeColor',[.9 1 .9])
+    plot(dateUS(Rt_epiestim_bystate(i).t_end'),Rt_bystate_epiestim(:,i),'Color',[0 .7 0],'Linewidth',1)
     plot(dateUS,ones(length(dateUS),1),'--k')
     if rem(i,6)==1
     ylabel('R_t')
@@ -400,9 +429,9 @@ for i=1:51
     ax(2).YLabel.String='% positive';
     end
     datetick('x','mmm-dd')
-    xlim([dateUS(1)-3 dateUS(end)+1])
+    xlim([dateUS(1)-3 dateUS(end)+2])
     ylim([0 15])
-    set(gca,'YTick',0:5:15,'XTick',[dateUS(1)-3 dateUS(120)],'XTickLabel',{'Mar1','Jul1'},'FontSize',8)
+    set(gca,'YTick',0:5:15,'XTick',[dateUS(1)-3 dateUS(90) dateUS(end)+2],'XTickLabel',{'Mar1','Jun1','Sep1'},'FontSize',8)
     title(states(i)) 
 end
     
@@ -416,14 +445,14 @@ set(ax(1),'YColor','k','FontSize',8)
 set(ax(2),'YColor','k','XLim',[dateUS(1)-3 dateUS(end)+1],'YLim',[0 100],'YTick',0:50:100,'FontSize',8)
 fill([dateUS(1:end-1,1); dateUS(end-1:-1:1,1); dateUS(1)],[Rt_US_ci(1:end,1); Rt_US_ci(end:-1:1,2); Rt_US_ci(1,1)],[.9 .9 .9],'EdgeColor',[.9 .9 .9])
 %plot(dateUS(1:end-1,1),Rt_US_tests_adj,'r')
-plot(dateUS(1:end-1,1),Rt_US,'Color',':k')
+plot(dateUS(1:end-1,1),Rt_US,':k')
 plot(dateUS(1:end-1,1),Rt_US_adj,'k')
 plot(dateUS(1:end-13,1),Rt_US(1:end-12,1),'Color',[0 .7 0],'LineWidth',1)
 plot(dateUS,ones(length(dateUS),1),'--k')
 ylabel('R_t')
 ax(2).YLabel.String='% positive';
 datetick('x','mmm')
-xlim([dateUS(1)-3 dateUS(end)-3])
+xlim([dateUS(1)-3 dateUS(end)+2])
 ylim([0 15])
-set(gca,'YTick',0:5:15,'XTick',[dateUS(1)-3 dateUS(120)],'XTickLabel',{'Mar1','Jul1'},'FontSize',8)
+set(gca,'YTick',0:5:15,'XTick',[dateUS(1)-3 dateUS(90) dateUS(end)+2],'XTickLabel',{'Mar1','Jun1','Sep1'},'FontSize',8)
 title('US')
